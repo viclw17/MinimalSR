59c7c12707b1e04ad11f8ae5f3d78b67c3f8a784
Victor Li <viclw17@gmail.com>
2021-02-09 22:47:57 -0800

    init commit

---
+++ MinimalSR/gl/main.cpp
@@ -0,0 +1,138 @@
+#include <vector>
+#include <iostream>
+
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
+#include "our_gl.h"
+
+Model *model     = NULL;
+const int width  = 800;
+const int height = 800;
+
+Vec3f light_dir(1,1,1);
+//Vec3f       eye(0,-1,3);
+Vec3f       eye(1, 1, 3);
+Vec3f    center(0,0,0);
+Vec3f        up(0,1,0);
+
+struct GouraudShader : public IShader {
+    Vec3f varying_intensity; // written by vertex shader, read by fragment shader
+
+    virtual Vec4f vertex(int iface, int nthvert) {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+        gl_Vertex = Viewport*Projection*ModelView*gl_Vertex;     // transform it to screen coordinates
+        varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert)*light_dir); // get diffuse lighting intensity
+        return gl_Vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color) {
+        float intensity = varying_intensity*bar;   // interpolate intensity for the current pixel
+        color = TGAColor(255, 255, 255)*intensity; // well duh
+        return false;                              // no, we do not discard this pixel
+    }
+};
+
+struct Shader : public IShader {
+	Vec3f          varying_intensity; // written by vertex shader, read by fragment shader
+	mat<2, 3, float> varying_uv;        // same as above
+
+	virtual Vec4f vertex(int iface, int nthvert) {
+		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		varying_intensity[nthvert] = std::max(0.f, model->normal(iface, nthvert) * light_dir); // get diffuse lighting intensity
+		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+		return Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
+	}
+
+	virtual bool fragment(Vec3f bar, TGAColor& color) {
+		float intensity = varying_intensity * bar;   // interpolate intensity for the current pixel
+		Vec2f uv = varying_uv * bar;                 // interpolate uv for the current pixel
+		color = model->diffuse(uv) * intensity;      // well duh
+		return false;                              // no, we do not discard this pixel
+	}
+};
+
+struct ShaderNormal : public IShader {
+	mat<2, 3, float> varying_uv;  // same as above
+	mat<4, 4, float> uniform_M;   //  Projection*ModelView
+	mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
+
+	virtual Vec4f vertex(int iface, int nthvert) {
+		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+		return Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
+	}
+
+	virtual bool fragment(Vec3f bar, TGAColor& color) {
+		Vec2f uv = varying_uv * bar;                 // interpolate uv for the current pixel
+		Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
+		Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
+		float intensity = std::max(0.f, n * l);
+		color = model->diffuse(uv) * intensity;      // well duh
+		return false;                              // no, we do not discard this pixel
+	}
+};
+
+struct ShaderSpec : public IShader {
+	mat<2, 3, float> varying_uv;  // same as above
+	mat<4, 4, float> uniform_M;   //  Projection*ModelView
+	mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
+
+	virtual Vec4f vertex(int iface, int nthvert) {
+		varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+		Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+		return Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
+	}
+
+	virtual bool fragment(Vec3f bar, TGAColor& color) {
+		Vec2f uv = varying_uv * bar;
+		Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
+		Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
+		Vec3f r = (n * (n * l * 2.f) - l).normalize();   // reflected light
+		float spec = pow(std::max(r.z, 0.0f), model->specular(uv));
+		float diff = std::max(0.f, n * l);
+		TGAColor c = model->diffuse(uv);
+		color = c;
+		for (int i = 0; i < 3; i++) color[i] = std::min<float>(5 + c[i] * (diff + .6 * spec), 255);
+		return false;
+	}
+};
+
+int main(int argc, char** argv) {
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        //model = new Model("obj/african_head.obj");
+		model = new Model("obj/ico.obj");
+    }
+
+    lookat(eye, center, up);
+    viewport(width/8, height/8, width*3/4, height*3/4);
+    projection(-1.f/(eye-center).norm());
+    light_dir.normalize();
+
+    TGAImage image  (width, height, TGAImage::RGB);
+    TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
+
+    GouraudShader shader;
+
+	//ShaderSpec shader;
+	//shader.uniform_M = Projection * ModelView;
+	//shader.uniform_MIT = (Projection * ModelView).invert_transpose();
+
+    for (int i=0; i<model->nfaces(); i++) {
+        Vec4f screen_coords[3];
+        for (int j=0; j<3; j++) {
+            screen_coords[j] = shader.vertex(i, j);
+        }
+        triangle(screen_coords, shader, image, zbuffer);
+    }
+
+    image.  flip_vertically(); // to place the origin in the bottom left corner of the image
+    zbuffer.flip_vertically();
+    image.  write_tga_file("output.tga");
+    zbuffer.write_tga_file("zbuffer.tga");
+
+    delete model;
+    return 0;
+}
